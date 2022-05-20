ea2c029c62e350f7f8d9d1fa9132c76ff998e8c2
evallina <eloyvallina33@gmail.com>
2022-05-20 16:03:08 +0200

    secret key from environment

--- dataExplorer/settings.py
+++ dataExplorer/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # set in heroku CONFIG VARS
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
